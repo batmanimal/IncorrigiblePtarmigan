d60cbbd142cfc588ade62ab49eb2b9158b1dafe2
batmanimal <thejenniekim@gmail.com>
2015-08-06 15:16:03 -0700

    Create guest model

--- client/models/GuestModel.js
+++ client/models/GuestModel.js
@@ -1,1 +1,19 @@
 /* create guest model in backbone */
+var Guest = Backbone.Model.extend({
+	defaults: {
+		// user input will be friendName, server looksup friendId from the db
+		friendName: '',
+		diningTableId: null,
+		constraints: []
+	},
+	initialize: function(){
+		
+	}
+});
+
+// var GuestSchema = mongoose.Schema({
+//   guestName: String,
+//   friendId: Number,
+//   diningTableId: Number,
+//   constraints: Array
+// });
